4546aad6a6e2a760595dd4396af1724b63fe8ae4
Faustini <101998529+youngfaustini@users.noreply.github.com>
2022-06-12 01:08:18 -0600

    Update I am Fausto.py

--- I am Fausto.py
+++ I am Fausto.py
@@ -10,5 +10,5 @@ print('')
 Write.Print("""About me? I'm from Costa Rica,
 Im 15y, I really like Python, my favorite language, I like GoLang, im still learning, about C# I know a little, and C++ 
 im still learning, i like HTML5 but, a bit tbh hahaha, and
-I handle Batchfile well too. Sorry for my bad english i try to improve it day to day!\n\n""", Colors.green_to_yellow, interval=0.03)
+I handle Batchfile well too. Sorry for my bad english i try to improve it day to day, bye!\n\n""", Colors.green_to_yellow, interval=0.03)
 os.system('pause')
